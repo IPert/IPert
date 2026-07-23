a2911f42cf615447d8ae4afc7ba8fbce8440a655
IPert <salmondp007@gmail.com>
2026-07-22 21:28:01 -1000

    Update script.js

--- script.js
+++ script.js
@@ -11,36 +11,42 @@ body{
     color:white;
 }
 
-.bg{
+
+.background{
+
     position:fixed;
+
     width:100%;
     height:100%;
 
     background:
     linear-gradient(
-        rgba(0,0,0,0.6),
+        rgba(0,0,0,0.7),
         rgba(0,0,0,0.9)
-    ),
-    url("background.jpg");
+    );
 
-    background-size:cover;
-    background-position:center;
+    background-color:#111;
+
 }
 
 
 .card{
 
     position:absolute;
+
     top:50%;
     left:50%;
+
     transform:translate(-50%,-50%);
 
     width:350px;
+
     padding:35px;
 
     text-align:center;
 
     background:rgba(255,255,255,0.1);
+
     backdrop-filter:blur(15px);
 
     border-radius:25px;
@@ -51,29 +57,49 @@ body{
 .profile{
 
     width:120px;
+
     height:120px;
 
-    border-radius:50%;
-    object-fit:cover;
+    margin:auto;
 
-    border:3px solid white;
+    display:flex;
+
+    justify-content:center;
+
+    align-items:center;
+
+    border-radius:50%;
+
+    background:white;
+
+    color:black;
+
+    font-size:40px;
+
+    font-weight:bold;
 
 }
 
 
 h1{
-    font-size:40px;
+
     margin-top:20px;
+
+    font-size:40px;
+
 }
 
 
 p{
-    opacity:0.7;
+
     margin:10px;
+
+    opacity:0.7;
+
 }
 
 
-.buttons a{
+a{
 
     display:block;
 
@@ -81,25 +107,21 @@ p{
 
     padding:15px;
 
+    border-radius:15px;
+
     background:rgba(255,255,255,0.15);
 
     color:white;
 
     text-decoration:none;
 
-    border-radius:15px;
-
-    transition:0.3s;
-
 }
 
 
-.buttons a:hover{
+a:hover{
 
     background:white;
 
     color:black;
-
-    transform:scale(1.05);
 
 }
