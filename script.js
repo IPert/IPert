1bf8f86b53cfb51c81888cd892c56ecf4a453dcc
IPert <salmondp007@gmail.com>
2026-07-23 01:22:23 -1000

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,26 @@ a{
     text-decoration:none;
     border-radius:10px;
 }
+
+const leaves=document.getElementById("leaves");
+
+for(let i=0;i<18;i++){
+
+let leaf=document.createElement("div");
+
+leaf.className="leaf";
+
+leaf.style.left=Math.random()100+"%";
+
+leaf.style.animationDuration=
+10+Math.random()12+"s";
+
+leaf.style.animationDelay=
+Math.random()*15+"s";
+
+leaf.style.transform=
+scale(${0.5+Math.random()});
+
+leaves.appendChild(leaf);
+
+}
